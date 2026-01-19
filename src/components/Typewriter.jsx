63cbf491c3f79a118852dedee9afabb363817ae8
Wenderson Carvalho <Wendersonmotarauj@gmail.com>
2026-01-19 13:39:53 -0300

    pedido

--- src/components/Typewriter.jsx
+++ src/components/Typewriter.jsx
@@ -1,45 +1,39 @@
 import { useEffect, useRef, useState } from 'react'
-import typeSound from '../assets/type.mp3'
+import typingSound from '../assets/typing.mp3'
 
 export default function Typewriter({ text, speed = 40, onFinish }) {
-  const [displayed, setDisplayed] = useState('')
-  const indexRef = useRef(0)
-  const audioRef = useRef(null)
-  
-   useEffect(() => {
-  const unlock = () => {
-    const audio = new Audio()
-    audio.play().catch(() => {})
-    window.removeEventListener('click', unlock)
-  }
+  const [displayedText, setDisplayedText] = useState('')
+  const [index, setIndex] = useState(0)
 
-  window.addEventListener('click', unlock)
-  return () => window.removeEventListener('click', unlock)
-}, [])
+  const audioRef = useRef(null)
 
   useEffect(() => {
-    audioRef.current = new Audio(typeSound)
-    audioRef.current.volume = 0.4
+    audioRef.current = new Audio(typingSound)
+    audioRef.current.volume = 0.2
   }, [])
 
   useEffect(() => {
-    if (indexRef.current >= text.length) {
-      onFinish?.()
-      return
+    if (index < text.length) {
+      const timeout = setTimeout(() => {
+        setDisplayedText(prev => prev + text[index])
+        setIndex(index + 1)
+
+        // toca som somente se não for espaço
+        if (text[index] !== ' ') {
+          audioRef.current.currentTime = 0
+          audioRef.current.play().catch(() => {})
+        }
+      }, speed)
+
+      return () => clearTimeout(timeout)
+    } else {
+      // Para o som ao finalizar o parágrafo
+      audioRef.current.pause()
+      audioRef.current.currentTime = 0
+
+      if (onFinish) onFinish()
     }
+  }, [index, text, speed, onFinish])
 
-    const timeout = setTimeout(() => {
-      setDisplayed(prev => prev + text[indexRef.current])
-      indexRef.current++
-
-      if (audioRef.current) {
-        audioRef.current.currentTime = 0
-        audioRef.current.play().catch(() => {})
-      }
-    }, speed)
-
-    return () => clearTimeout(timeout)
-  }, [displayed])
-
-  return <p>{displayed}</p>
+  return <p>{displayedText}</p>
 }
